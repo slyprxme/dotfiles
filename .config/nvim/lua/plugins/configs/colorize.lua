6bb08e078dcca785877727ae306fc93c511e4bee
Slyprxme <sly.prxme@gmail.com>
2023-05-21 21:54:19 +0530

    config update

--- .config/nvim/lua/plugins/configs/colorize.lua
+++ .config/nvim/lua/plugins/configs/colorize.lua
@@ -8,13 +8,23 @@ colorizer.setup({
 	user_default_options = {
 		RGB = true, -- #RGB hex codes
 		RRGGBB = true, -- #RRGGBB hex codes
-		names = false, -- "Name" codes like Blue
+		names = true, -- "Name" codes like Blue or blue
 		RRGGBBAA = true, -- #RRGGBBAA hex codes
+		AARRGGBB = true, -- 0xAARRGGBB hex codes
 		rgb_fn = true, -- CSS rgb() and rgba() functions
 		hsl_fn = true, -- CSS hsl() and hsla() functions
-		css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+		css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
 		css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-		-- Available modes: foreground, background, virtualtext
-		mode = "background", -- Set the display mode.)
+		-- Available modes for `mode`: foreground, background,  virtualtext
+		mode = "virtualtext", -- Set the display mode.
+		-- Available methods are false / true / "normal" / "lsp" / "both"
+		-- True is same as normal
+		tailwind = false, -- Enable tailwind colors
+		-- parsers can contain values used in |user_default_options|
+		sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+		virtualtext = "",
+		-- update color values even if buffer is not focused
+		-- example use: cmp_menu, cmp_docs
+		always_update = false,
 	},
 })
