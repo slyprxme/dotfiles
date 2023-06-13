c819cdec1e191d7b7edfc198b339184f3bfa0246
Slyprxme <sly.prxme@gmail.com>
2023-06-13 23:02:03 +0530

    config update

--- .config/nvim/lua/plugins/configs/colorscheme.lua
+++ .config/nvim/lua/plugins/configs/colorscheme.lua
@@ -4,10 +4,17 @@ vim.cmd("colorscheme onedark")
 -- 	palettes = {
 -- 		nordfox = {
 -- 			bg1 = "#282c34",
--- 			bg0 = "#1f2131",
--- 			bg3 = "#1f2131",
+-- 			red = "#e06c75",
+-- 			orange = "#d19a66",
+-- 			yellow = "#e5c07b",
+-- 			green = "#98c379",
+-- 			cyan = "#56b6c2",
+-- 			blue = "#61afef",
+-- 			purple = "#c678dd",
+-- 			white = "#abb2bf",
+-- 			black = "#282c34",
+-- 			gray = "#5c6370",
 -- 		},
 -- 	},
 -- })
--- --
 -- vim.cmd("colorscheme nordfox")
