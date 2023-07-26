a81ce23fb22e21e813c74f846435dfcdf6591690
Slyprxme <sly.prxme@gmail.com>
2023-07-26 23:30:08 +0530

    colorscheme update

--- .config/nvim/lua/plugins/configs/colorscheme.lua
+++ .config/nvim/lua/plugins/configs/colorscheme.lua
@@ -14,34 +14,17 @@ require("onedarkpro").setup({
 --       -- highlight = "#e2be7d",
 --       -- comment = "#7f848e",
     },
---   }, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
---   -- highlights = {
---   -- ["@field.lua"] = { fg = "${cyan}" },
---   -- }, -- Override default highlight and/or filetype groups
---   -- filetypes = { -- Override which filetype highlight groups are loaded
---   -- 	javascript = true,
---   -- 	lua = true,
---   -- 	markdown = true,
---   -- 	php = true,
---   -- 	python = true,
---   -- 	ruby = true,
---   -- 	rust = true,
---   -- 	toml = true,
---   -- 	typescript = true,
---   -- 	typescriptreact = true,
---   -- 	vue = true,
---   -- 	yaml = true,
   },
-  options = {
+  -- options = {
 --     bold = true,                  -- Use bold styles?
 --     italic = true,                -- Use italic styles?
 --     underline = true,             -- Use underline styles?
 --     undercurl = true,             -- Use undercurl styles?
 --     cursorline = true,            -- Use cursorline highlighting?
-    transparency = false,            -- Use a transparent background?
+    -- transparency = false,            -- Use a transparent background?
 --     terminal_colors = true,       -- Use the theme's colors for Neovim's :terminal?
 --     window_unfocused_color = false, -- When the window is out of focus, change the normal background?
-  },
+  -- },
 })
 vim.cmd("colorscheme onedark")
 
