3bc40ebe15aa6fee64467490cff6253270caf669
Slyprxme <sly.prxme@gmail.com>
2023-08-04 20:17:25 +0530

    config update

--- .config/nvim/lua/plugins/configs/colorize.lua
+++ .config/nvim/lua/plugins/configs/colorize.lua
@@ -16,13 +16,13 @@ colorizer.setup({
 		css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
 		css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 		-- Available modes for `mode`: foreground, background,  virtualtext
-		mode = "virtualtext", -- Set the display mode.
+		mode = "background", -- Set the display mode.
 		-- Available methods are false / true / "normal" / "lsp" / "both"
 		-- True is same as normal
 		tailwind = false, -- Enable tailwind colors
 		-- parsers can contain values used in |user_default_options|
 		sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
-		virtualtext = "",
+		virtualtext = "󰚍",
 		-- update color values even if buffer is not focused
 		-- example use: cmp_menu, cmp_docs
 		always_update = false,
